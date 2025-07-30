deeeae062c7b142d3ec1266593719f8a95cba7fc
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-07-30 17:44:47 +0530

    frontend fix 2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,6 +11,16 @@ const nextConfig = {
       },
     ];
   },
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
