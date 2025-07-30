bd3a659e9e1cbed94a08022f4a2f030d65c8ef85
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-07-30 20:32:39 +0530

    removal of useless stuff

--- frontend/src/components/layouts/DashboardLayout.tsx
+++ frontend/src/components/layouts/DashboardLayout.tsx
@@ -200,17 +200,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
               >
                 <Menu className="h-6 w-6" />
               </button>
-              
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Search className="h-5 w-5 text-gray-400" />
-                </div>
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className="block w-full pl-10 pr-3 py-2 border border-gray-200 dark:border-gray-600 rounded-xl bg-white dark:bg-gray-700 text-gray-900 dark:text-white placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-transparent"
-                />
-              </div>
+              {/* Search bar removed */}
             </div>
 
             <div className="flex items-center space-x-4">
